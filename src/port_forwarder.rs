e2431d0a3fbfe75197a0819231c3afc93f2693f6
pzmarzly <pawo2500@gmail.com>
2024-07-23 08:14:32 +0100

    update dependencies

--- src/port_forwarder.rs
+++ src/port_forwarder.rs
@@ -1,17 +1,17 @@
 // Copyright (c) 2018 Paweł Zmarzły
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-use igd::{ self, SearchError, AddAnyPortError, AddPortError, RemovePortError };
-use std::net::{ SocketAddrV4, Ipv4Addr };
+use igd::{self, AddAnyPortError, AddPortError, RemovePortError, SearchError, SearchOptions};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
 pub use igd::PortMappingProtocol;
 
 #[derive(Debug)]
 pub struct Port {
     pub proto: PortMappingProtocol,
-    pub num: u16
+    pub num: u16,
 }
 
 impl PartialEq for Port {
@@ -24,53 +24,96 @@ impl PartialEq for Port {
 pub struct Forwarder {
     pub gateway: igd::Gateway,
     pub network_interface: Ipv4Addr,
-    pub open_ports: Vec<Port>
+    pub open_ports: Vec<Port>,
 }
 
 pub fn create_forwarder(interface_ip: Ipv4Addr) -> Result<Forwarder, SearchError> {
-    igd::search_gateway_from(interface_ip).map(|gateway|
-        Forwarder {
-            gateway: gateway,
-            network_interface: interface_ip,
-            open_ports: Vec::new()
-        }
-    )
+    igd::search_gateway(SearchOptions {
+        bind_addr: SocketAddr::V4(SocketAddrV4::new(interface_ip, 0)),
+        ..Default::default()
+    })
+    .map(|gateway| Forwarder {
+        gateway: gateway,
+        network_interface: interface_ip,
+        open_ports: Vec::new(),
+    })
 }
 
 pub fn create_forwarder_from_any<I>(interface_ips: I) -> Result<Forwarder, Vec<SearchError>>
-    where
-        I: IntoIterator<Item = Ipv4Addr>, {
+where
+    I: IntoIterator<Item = Ipv4Addr>,
+{
     let mut errors = Vec::new();
     for interface_ip in interface_ips {
         match create_forwarder(interface_ip) {
             Ok(forwarder) => return Ok(forwarder),
-            Err(error) => errors.push(error)
+            Err(error) => errors.push(error),
         }
     }
     Err(errors)
 }
 
 impl Forwarder {
-    pub fn forward_any_port(&mut self, local_port: u16, proto: PortMappingProtocol, name: &str) -> Result<u16, AddAnyPortError> {
+    pub fn forward_any_port(
+        &mut self,
+        local_port: u16,
+        proto: PortMappingProtocol,
+        name: &str,
+    ) -> Result<u16, AddAnyPortError> {
         self.gateway
-            .add_any_port(proto, SocketAddrV4::new(self.network_interface, local_port), 0, name)
+            .add_any_port(
+                proto,
+                SocketAddrV4::new(self.network_interface, local_port),
+                0,
+                name,
+            )
             .map(|port| {
-                self.open_ports.push(Port { proto: proto, num: port });
+                self.open_ports.push(Port {
+                    proto: proto,
+                    num: port,
+                });
                 port
             })
     }
-    pub fn forward_port(&mut self, local_port: u16, remote_port: u16, proto: PortMappingProtocol, name: &str) -> Result<(), AddPortError> {
+    pub fn forward_port(
+        &mut self,
+        local_port: u16,
+        remote_port: u16,
+        proto: PortMappingProtocol,
+        name: &str,
+    ) -> Result<(), AddPortError> {
         self.gateway
-            .add_port(proto, remote_port, SocketAddrV4::new(self.network_interface, local_port), 0, name)
+            .add_port(
+                proto,
+                remote_port,
+                SocketAddrV4::new(self.network_interface, local_port),
+                0,
+                name,
+            )
             .map(|()| {
-                self.open_ports.push(Port { proto: proto, num: remote_port });
+                self.open_ports.push(Port {
+                    proto: proto,
+                    num: remote_port,
+                });
             })
     }
-    pub fn remove_port(&mut self, remote_port: u16, proto: PortMappingProtocol) -> Result<(), RemovePortError> {
-        if let Some(pos) = self.open_ports.iter().position(|el| *el == Port { proto: proto, num: remote_port }) {
+    pub fn remove_port(
+        &mut self,
+        remote_port: u16,
+        proto: PortMappingProtocol,
+    ) -> Result<(), RemovePortError> {
+        if let Some(pos) = self.open_ports.iter().position(|el| {
+            *el == Port {
+                proto: proto,
+                num: remote_port,
+            }
+        }) {
             self.open_ports.remove(pos);
         } else {
-            println!("Remote port {} {} was not opened by this Forwarder! Removing anyway...", proto, remote_port);
+            println!(
+                "Remote port {} {} was not opened by this Forwarder! Removing anyway...",
+                proto, remote_port
+            );
         }
         self.gateway.remove_port(proto, remote_port)
     }
